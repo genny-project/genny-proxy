f898510f030a6f60b2d1dede36e5d6717a13716c
Byron Andres <heliosphanes@outlook.com>
2020-10-08 19:04:52 +1100

    add way to handle abn key when it is not provided

--- src/main/java/life/genny/abn/AbnLookup.java
+++ src/main/java/life/genny/abn/AbnLookup.java
@@ -1,12 +1,19 @@
 package life.genny.abn;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.xml.parsers.FactoryConfigurationError;
@@ -17,8 +24,26 @@ public class AbnLookup {
 
   static final String UTF_8 = "UTF-8";
   static final String callbackName = "c";
-  static final String abnKey = System.getenv("ABN_KEY");
 
+  static final String ABN_KEY_FILE_PATH = "/tmp/abn-key";
+  static Predicate<String> isNotEmpty = a -> !a.isEmpty();
+
+  static Supplier<String> readKeyFromFileSystem = () -> {
+    String data = "";
+    File myObj = new File(ABN_KEY_FILE_PATH);
+    try (Scanner myReader = new Scanner(myObj)){
+      data = myReader.nextLine();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    return data;
+  };
+
+  static final String abnKey = Optional.ofNullable(System.getenv("ABN_KEY"))
+      .filter(Objects::nonNull)
+      .filter(isNotEmpty)
+      .orElseGet(readKeyFromFileSystem);
+  
   static String getCallbackJson(String callbackResult) {
     String jsonRes = callbackResult.substring(callbackName.length() + 1, callbackResult.length() - 1);
     return jsonRes;
